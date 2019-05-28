db9e00bc5123cc2e9bfa3745c06f6335122daaed
Unknown <martinezrequenamario@gmail.com>
2019-05-28 18:09:15 +0200

    new and improve exec.py
    more functionalities, tested and working

--- Scripts/exec.py
+++ Scripts/exec.py
@@ -1,14 +1,17 @@
 import csv
 import os
+import numpy as np
+from timeit import default_timer as timer
 
 # returns the execution time of one log. file
 def extract_exec_time(filename):
     with open (filename) as f:
         lines = reversed(f.readlines())#.reverse()
         for line in lines:
-            if line.startswith("ExecutionTime"):
-                return line.split("=")[1].split("s")[0]
+            if line.startswith("ExecutionTime") or line.startswith("Finished meshing in"):
+                return str(line.split("=")[1].split("s")[0].strip())
         return -1
+
 
 # counts the number of entries in specified csv
 def count_rows(filename):
@@ -49,30 +52,44 @@ def read_allrun_decomposeParDict():
 
 # if the decomposepardict exists, it is deleted and substituted with the new one.
 # it creates new files from a template decomposepardict
-def write_decomposeParDict(number,orig):
+def write_decomposeParDict(number,orig=None):
+    number=str(number)
     if orig is None:
         orig="decomposeParDict"
     if os.path.isfile("/system/"+orig+"."+number):
-        os.system("rm sytem/"+orig+"."+number)
-    os.system("cp system/"+orig+" sytem/"+orig+"."+number)
+        os.system("rm -f sytem/"+orig+"."+number)
+
+    os.system("cp system/"+orig+" system/"+orig+"."+number)
     modify_file(orig+"."+number, "numberOfSubdomains", str(number))
 
-
 # modifies file filename, changing variable to the specified value
-def modify_file(filename, variable, value):
+# if line_number is provided, it will modify that specific line
+def modify_file(filename, variable, value,line_number=None):
     with open("system/"+filename , 'r+b') as f:
         lines = f.readlines()
-        for idx, line in enumerate(lines):
-            if variable in line and "//" not in line:
-                line = line.split(variable + " ")[0]+variable+" "+value+";\n"
-                lines[idx]=line
-                print("File modified. Variable '"+variable+"' set up as '"+value+"'")
-                f.seek(0, 0)
-                f.truncate()
-                f.write("".join(lines))
-                f.close()
-                return
-        return -1
+
+        newline=None
+        print line_number
+        if line_number is None:
+            for idx, line in enumerate(lines):
+                if variable in line and "//" not in line:
+                    newline = line.split(variable + " ")[0]+variable+" "+str(value)+";\n"
+                    lines[idx]=newline
+                    break
+        else:
+            line_number-=1
+            newline = lines[line_number].split(variable + " ")[0]+variable+" "+str(value)+";\n"
+            lines[line_number]=newline
+        
+        if newline is None:
+            return -1
+        else:   
+            print("File modified. Variable '"+variable+"' set up as '"+str(value)+"'")
+            f.seek(0, 0)
+            f.truncate()
+            f.write("".join(lines))
+            f.close()
+            return
     return -1
 
 # excutes without arguments, recording info contained in various log files
@@ -123,8 +140,8 @@ def execution_raw():
                 another_line=another_line+","+time
                 auxtime=auxtime+float(time)
         if row_count==0:
-            table.write(first_line+",readtime\n")
-        table.write(another_line+","+str(auxtime)+"\n")
+            table.write(first_line+",readtime")
+        table.write("\n"+another_line+","+str(auxtime))
         print another_line
     print("\nCSV file modified/created")
 
@@ -132,12 +149,17 @@ def execution_raw():
 # the idea behind this function is that it will invoque the modification of the function
 # and then clean and execute the foam experiment. After completition, it will record the excution time 
 # of the whole execution, simple and potential foam, and what have been modified in for this execution. 
-def exec_with_modification(output_filename,filename, variable, value, message):
-    print("\nBegining execution\n")
-    print("Printint to "+output_filename+" . Changes to "+filename+" , variable: '"+variable+"', value: "+value+"\n\n")
-    if modify_file(filename, variable, value) is -1:
-        print "Error modifying the file. It does not exist or the variable cannot be found."
-        return 1
+# the variables message, line and decomp are optional
+def exec_with_modification(output_filename,filename, variable, value, message=None,line=None,decomp=-1):
+    print("\nCleaning and begining execution\n")
+    print("Printing to "+output_filename+".csv. Changes to "+filename+" , variable: '"+variable+"', value: "+str(value)+"\n\n")
+    if decomp is -1:
+        if modify_file(filename, variable, value) is -1:
+            print "Error modifying the file. It does not exist or the variable cannot be found."
+            return 1
+    else:
+        modify_allrun_decomposeParDict(decomp)
+    
     os.system("./Allclean")
     start = timer()
     os.system("./Allrun")
@@ -148,7 +170,7 @@ def exec_with_modification(output_filename,filename, variable, value, message):
     
     with open (output_filename+'.csv', 'a') as table:
         row_count = count_rows(output_filename+'.csv')
-        first_line="Number,Message,MeasuredTime,procNum,numCells,potentialFoam,simpleFoam,fileModified,Variable,newValue"
+        first_line="Number,Message,MeasuredTime,procNum,numCells,potentialFoam,simpleFoam,combineP+S,snappyHexMesh,fileModified,Variable,newValue"
         # Counting the number of experiments. 
         if row_count==0:
             another_line = "1"
@@ -159,30 +181,29 @@ def exec_with_modification(output_filename,filename, variable, value, message):
         if message is None:
             another_line+=','
         else:
-            another_line+=','+message
+            another_line+=',"'+message+'"'
         
         # total execution time, number of proc used and number of cells
         another_line+=','+measured+','+read_allrun_decomposeParDict()+','+get_num_cells()
 
         # adding execution times for potetialFoam and simpleFoam. If more variables wanted to be added,
         # this is when it should be done
-        another_line+=','+extract_exec_time("log.potentialFoam")+','+extract_exec_time("log.simpleFoam")
+        another_line+=','+extract_exec_time("log.potentialFoam")+','+extract_exec_time("log.simpleFoam")+','+str(float(extract_exec_time("log.potentialFoam"))+ float(extract_exec_time("log.simpleFoam")))+','+extract_exec_time("log.snappyHexMesh")
 
         # variables modified for in this execution
-        another_line+=','+filename+','+variable+','+value
+        another_line+=','+filename+','+variable+',"'+str(value)+'"'
         if row_count==0:
             table.write(first_line)
-        table.write(another_line)
+        table.write("\n"+another_line)
     print("\nCSV file modified/created")
 
 
 
 def main():
-#     modify_file("snappyHexMeshDict-dummy","maxLocalCells","200000")
-    # get_num_cells()
-    # exec_with_modification("test","snappyHexMeshDict-dummy","maxLocalCells",10000,"first execution")
-    # modify_allrun_decomposeParDict("23")
-    modify_allrun_decomposeParDict(21)
+    # exec_with_modification(output_filename,filename, variable, value, message=None,line=None,decomp=-1)
+    for i in range(6,26,2):
+        exec_with_modification("orig_proc_number","allrun","decomposeParDict",i,"Experiment with original meshing testing more procs",None,i)
+
 if __name__ == "__main__":
     main()
     
